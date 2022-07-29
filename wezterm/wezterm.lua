10295ab41180741e268d91ded5d190806420b689
Artorius <dwxd@foxmail.com>
2022-07-29 22:14:30 +0800

    change font setting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,16 +1,26 @@
 local color = require("colors.idleToes")
 local keybinds = require("keybinds")
+local wezterm = require("wezterm")
 
 return {
 	colors = color,
- 	keys = keybinds.create_keybinds(),
+	keys = keybinds.create_keybinds(),
 	key_tables = keybinds.key_tables,
-  window_frame = require "window_frame",
-	font_size = 14.0,
-  use_fancy_tab_bar = true,
-  -- 输入法
-  use_ime = true,
-  hide_tab_bar_if_only_one_tab = true,
+	window_frame = require("window_frame"),
+
+  -- font
+	font = wezterm.font("JetbrainsMono Nerd font"),
+	font_size = 15.0,
+
+	-- 输入法
+	use_ime = true,
+
+  -- tab bar
+	use_fancy_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	tab_bar_at_bottom = false,
+
+  -- window config
 	window_padding = {
 		left = 0,
 		right = 0,
