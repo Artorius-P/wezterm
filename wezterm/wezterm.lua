164ec4517d5e1d88bd65562c9b6e4b6b38492a2d
Artorius <dwxd@foxmail.com>
2022-07-29 21:59:57 +0800

    add keybinds

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,10 @@
 local color = require("colors.idleToes")
+local keybinds = require("keybinds")
 
 return {
 	colors = color,
+ 	keys = keybinds.create_keybinds(),
+	key_tables = keybinds.key_tables,
   window_frame = require "window_frame",
 	font_size = 14.0,
   use_fancy_tab_bar = true,
